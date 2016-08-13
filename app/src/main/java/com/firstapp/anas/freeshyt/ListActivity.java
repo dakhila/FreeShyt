33ad5fc00c580ea0ece29b9cf2ce58e9009ea383
Devin Stoen <Devin Stoen>
2016-08-13 03:48:13 -0700

    Free stuff list activity work

---
+++ app/src/main/java/com/firstapp/anas/freeshyt/ListActivity.java
@@ -0,0 +1,38 @@
+package com.firstapp.anas.freeshyt;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+public class ListActivity extends AppCompatActivity {
+
+
+    public MediaPlayer comfortable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.container, new ItemListFragment())
+                .commit();
+
+        comfortable = MediaPlayer.create(this, R.raw.comfortable);
+        comfortable.start();
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.list, menu);
+//        return true;
+//    }
+
+
+
+}
