d7844744ea4622ddc041a598fb8cb39dfbd3a459
Devin Stoen <Devin Stoen>
2016-08-19 23:26:31 -0700

    List Activity

--- app/src/main/java/com/firstapp/anas/freeshyt/MainActivity.java
+++ app/src/main/java/com/firstapp/anas/freeshyt/MainActivity.java
@@ -2,14 +2,13 @@ package com.firstapp.anas.freeshyt;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -104,7 +103,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         progressDialog.dismiss();
                     }
                 });
-
     }
 
     @Override
