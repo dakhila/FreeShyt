5fb3e98a982199e50ecd7ad770d578c6609758b8
Anas <dakhila@uw.edu>
2016-08-20 01:58:35 -0700

    linked camera to profile page

--- app/src/main/java/com/firstapp/anas/freeshyt/CameraActivity.java
+++ app/src/main/java/com/firstapp/anas/freeshyt/CameraActivity.java
@@ -8,43 +8,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
-
-    private FirebaseAuth firebaseAuth;
+public class CameraActivity extends AppCompatActivity implements View.OnClickListener{
     ImageView iv;
-    private TextView textViewUserEmail;
-    private Button buttonLogout;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_camera);
 
         iv = (ImageView)findViewById(R.id.imageView);
 
         Button buttonphoto = (Button) findViewById(R.id.buttonPhoto);
         buttonphoto.setOnClickListener(this);
-
-        firebaseAuth = FirebaseAuth.getInstance();
-
-        if (firebaseAuth.getCurrentUser() == null) {
-            finish();
-            startActivity(new Intent(this, LoginActivity.class));
-        }
-
-        FirebaseUser user = firebaseAuth.getCurrentUser();
-
-        textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
-
-        textViewUserEmail.setText("Welcome " + user.getEmail());
-        buttonLogout = (Button) findViewById(R.id.buttonLogout);
-
-        buttonLogout.setOnClickListener(this);
     }
 
     @Override
@@ -52,12 +27,9 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, 0);
 
-        if (view == buttonLogout) {
-            firebaseAuth.signOut();
-            finish();
-            startActivity(new Intent(this,LoginActivity.class));
-        }
+
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
